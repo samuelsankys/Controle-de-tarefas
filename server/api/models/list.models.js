46f6276f3d491c7629b5369a82f4728b05450118
samuelsankys <ssankys3@gmail.com>
2022-06-09 20:58:26 -0300

    feat: create entity model

---
+++ server/api/models/list.models.js
@@ -0,0 +1,29 @@
+const Sequelize = require('sequelize');
+const db = require('../../config/database');
+
+const List = db.define('lists', { 
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      status: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at:{
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at:{
+        type: Sequelize.DATE,
+        allowNull: false,
+      }
+});
+
+module.exports = List
