6c4b8dd340f9e2a4b23b3632ae930f99c908b797
samuelsankys <ssankys3@gmail.com>
2022-06-09 21:29:51 -0300

    feat: add header server

--- server/app.js
+++ server/app.js
@@ -3,6 +3,18 @@ const app = express();
 const router = express.Router();
 
 
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET', 'POST', 'PUT', 'DELETE');
+    next();
+});
+
+
 //app.use('/', index);
 
 
