a4cdbdafbf07a1eaa127be6fc6363f5033984289
samuelsankys <ssankys3@gmail.com>
2022-06-14 14:56:03 -0300

    feat: create route get one Activity

--- server/api/routes/activities.routes.js
+++ server/api/routes/activities.routes.js
@@ -4,6 +4,7 @@ const router = require('express').Router();
 
 
 router.get('/:list_id/activity', controller.getActivitiesList);
+router.get('/:list_id/activity/:activity_id', controller.getActivity);
 router.post('/:list_id/activity', controller.createActivity);
 router.put('/:list_id/activity/:activity_id', controller.updateActivity);
 router.delete('/:list_id/activity/:activity_id', controller.deleteActivity);
